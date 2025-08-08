e0d4f27d922af3ff3195f7dabb68c36f3751f443
podong1969-sys <podong1969@gmail.com>
2025-08-08 12:19:37 +0900

    lumi.ts

---
+++ lumi.ts
@@ -0,0 +1,9 @@
+
+import { createClient } from '@lumi.new/sdk'
+
+export const lumi = createClient({
+  projectId: 'p344312510826868736',
+  apiBaseUrl: 'https://api.lumi.new',
+  authOrigin: 'https://auth.lumi.new',
+})
+
